50b54a8c46623749fb2b5ab05a28863a26cc525d
KristofferAandahl <kristoffer.aandahl@gmail.com>
2024-10-30 10:27:29 +0100

    changed to a dcr spesific converter

--- dcr.py
+++ dcr.py
@@ -2,8 +2,8 @@ import pm4py
 import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning) 
 
-if __name__ == "__main__":
-    log = pm4py.read_xes('rounds_data.xes')
-    print(log)
-    graph, _ = pm4py.discover_dcr(log,post_process=["roles", "pending"],resource_key="org:role",group_key="org:role")
-    pm4py.view_dcr(graph)
+log = pm4py.read_xes('rounds_data.xes')
+print(log)
+graph, _ = pm4py.discover_dcr(log, post_process=["roles", "pending"], resource_key="org:role",group_key="org:role")
+print("Found graph")
+pm4py.view_dcr(graph)
